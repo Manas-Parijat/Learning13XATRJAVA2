393c8ed8312b30907774f995b76b3c5ba91d1bf1
Manas-Parijat <parijatmanas@gmail.com>
2025-06-13 17:00:57 +0530

    NewFile

---
+++ src/Ex001_Variable.java
@@ -0,0 +1,16 @@
+public class Ex001_Variable {
+    public static void main(String[] args) {
+        // Declare variables
+        int age = 25;
+        String name = "John";
+        double height = 5.9;
+        boolean isStudent = true;
+
+        // Print variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Is Student? " + isStudent);
+    }
+}
+    
---
+++ src/Ex002_VariableINT.java
@@ -0,0 +1,12 @@
+public class Ex002_VariableINT {
+    public static void main(String[] args) {
+        int age = 23;
+        String Name = "";
+
+        System.out.println("age:" + age );
+        System.out.println("Name:" + "Parijat");
+
+
+
+    }
+}
